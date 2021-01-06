2efa4d9411d343ab46141aebcdbbaf3b5160a89a
Yan <61377352+wanuchar@users.noreply.github.com>
2021-01-06 23:33:43 +0300

    Delete Options.cs

--- Lab5/FileManager/OptionModels/Options.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FileManager.OptionModels
-{
-    public static class Options
-    {
-        public static PathOptions PathOptions;
-        public static ServiceOptions ServiceOptions;
-    }
-}
