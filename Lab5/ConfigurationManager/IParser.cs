72461fea2a50ec0db65a1da7e58e36cf3e5712d9
Yan <61377352+wanuchar@users.noreply.github.com>
2021-01-06 23:32:22 +0300

    Delete IParser.cs

--- Lab5/ConfigurationManager/IParser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConfigurationManager
-{
-    interface IParser<TOptionsModel> where TOptionsModel : class, new()
-    {
-        string Extension { get; }
-        TOptionsModel Parse(string path);
-        void Serialize(TOptionsModel configureObject, string path);
-    }
-}
